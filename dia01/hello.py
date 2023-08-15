2b39802ef28b27c66deedd5d4d83e420d908a23d
Ilan Ramos <ilanramos93@gmail.com>
2023-08-15 19:53:47 -0300

    initial steps on hello.py code

--- dia01/hello.py
+++ dia01/hello.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+"""Hello world multi-language
 
-# Este programa imprime hello world
-print("Hello, world!") 
+According to the language that is configured on the environment, the program will give the proper "Hello world" message.
+
+Usage: 
+It must have env variable accordingly configured
+Eg: export LANG=pt_BR
+
+Execution: 
+- python3 hello.py
+- ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Ilan Ramos"
+__license__ = "Unlicense"
+
+if __name__ == "__main__":
+    print("Hello, world!") 

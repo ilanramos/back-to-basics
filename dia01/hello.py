085e7823d095a6f12b5fd73fc08f87f44330acdb
Ilan Ramos <ilanramos93@gmail.com>
2023-08-19 18:45:01 -0300

    refactor hello.py

--- dia01/hello.py
+++ dia01/hello.py
@@ -6,18 +6,35 @@ According to the language that is configured on the environment, the program wil
 Usage: 
 It must have env variable accordingly configured
 Eg: export LANG=pt_BR
+Or inform through CLI argument `--lang`
 
 Execution: 
 - python3 hello.py
 - ./hello.py
 """
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Ilan Ramos"
 __license__ = "Unlicense"
 
 import os
+import sys
 
-current_language = os.getenv("LANG", "pt_BR.UTF-8")[:5] # snake case
+arguments = {
+    "lang": os.getenv("LANG", "pt_BR.UTF-8")[:5],
+    "count": 1,
+}
+
+for index, arg in enumerate(sys.argv[1:]):
+    # TODO: Tratar ValueError
+    arg = arg.lstrip('-')
+    if index % 2 == 0 and arg not in arguments.keys():
+        print(f"Comando `{arg}` não reconhecido")
+        sys.exit()
+
+    if arg in arguments.keys():
+        arguments[arg] = sys.argv[index + 2]
+
+current_language =  arguments['lang'] # snake case
 
 msg = {
     "en_US": "Hello, world!",
@@ -27,4 +44,5 @@ msg = {
     "fr_FR": "Bonjour, monde!",
 }
 
-print(msg.get(current_language, "Hello, world!"))
+print(
+    msg.get(current_language, "Hello, world!") * int(arguments['count']))

9556b44518fa4bcbd98d517724ee7fd685f66882
Ilan Ramos <ilanramos93@gmail.com>
2023-08-20 01:49:39 -0300

    prefix calc exercise

---
+++ dia03/prefixcalc.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""Prefix Calculator
+
+User will send via terminal the operation then the numbers
+
+[operation] [n1] [n2]
+
+Operations:
+sum -> +
+sub -> -
+mul -> *
+div -> /
+
+Eg:
+$ prefixcalc.py sum 5 2
+7 
+
+$ prefixcalc.py mul 10 5
+50
+
+$ prefixcalc.py 
+operation: sum
+n1: 5
+n2: 4
+9
+"""
+__version__ = "0.1.0"
+__author__ = "Ilan Ramos"
+
+import sys
+
+cli_arguments = sys.argv
+
+nums = []
+
+if len(cli_arguments) > 4:
+    print("Too many arguments. Eg. `sum 2 3`")
+    sys.exit(1)
+elif len(cli_arguments) == 1:
+    op = input("Operation: ")
+    nums.append(input("n1: "))
+    nums.append(input("n2: "))
+elif len(cli_arguments) == 4:
+    op = cli_arguments[1]
+    nums.append(cli_arguments[2])
+    nums.append(cli_arguments[3])
+else:
+    print("Arguments are missing. Eg. `sum 2 3`")
+    sys.exit(1)
+
+validated_nums = []
+
+for num in nums:
+    if num.isdigit():
+        validated_nums.append(int(num))
+    elif num.replace('.', '').isdigit():
+        validated_nums.append(float(num))
+    else:
+        print("Invalid number")
+        sys.exit(1)
+
+if op == "sum":
+    result = validated_nums[0] + validated_nums[1]
+elif op == "sub":
+    result = validated_nums[0] - validated_nums[1]
+elif op == "mul":
+    result = validated_nums[0] * validated_nums[1]
+elif op == "div":
+    result = validated_nums[0] / validated_nums[1]
+else:
+    print("Invalid operation\nValid operations: sum, sub, mul, div")
+    sys.exit(1)
+
+print(result)
